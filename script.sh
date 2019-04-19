743c64aa97614723817356b051cca17c6645e6ae
IKhonakhbeeva <i.khonakhbeeva@gmail.com>
2019-04-19 21:53:34 +0300

    more equal

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ set -o errexit
 cd "$(dirname "$0")"
 
 cache_dir=$(cygpath -m $APPDATA | xargs cygpath)/trik-studio/installer_cache
-mkdir $cache_dir/winscp
+mkdir -p $cache_dir/winscp
 echo $cache_dir
 echo download:
 curl -L -s -o $cache_dir/winscp.zip https://sourceforge.net/projects/winscp/files/WinSCP/5.15/WinSCP-5.15-Portable.zip
